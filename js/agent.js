cae5a90fd485b051d12eb5a766664b7ba301ad3f
Pat Lillis <lillispm@gmail.com>
2016-11-03 22:36:27 -0600

    Add colors and better behavior

--- js/agent.js
+++ js/agent.js
@@ -1,5 +1,5 @@
 class Agent {
-    constructor(simplex, alea, canvas) {
+    constructor(simplex, alea, canvas, palette) {
         this.simplex = simplex;
         this.alea = alea;
         this.canvas = canvas;
@@ -8,11 +8,13 @@ class Agent {
         this.x = this.alea();
         this.y = this.alea();
 
-        this.speed = 0.01 * this.alea();
+        this.speed = 0.005 * this.alea() + 0.001;
         this.resistance = this.alea();
 
         this.pastX = [];
         this.pastY = [];
+
+        this.color = palette[Math.floor(palette.length * this.alea())];
     }
 
     update(offsetX, offsetY) {
@@ -28,8 +30,8 @@ class Agent {
         this.y += Math.sin(angle) * speed * (1 - this.resistance);
         this.rotation = pixel * 360;
 
-        this.x = Agent.mod(this.x, 1.0);
-        this.y = Agent.mod(this.y, 1.0);
+        this.x = this.mod(this.x, 1.0);
+        this.y = this.mod(this.y, 1.0);
 
         this.pastX.push(this.x);
         this.pastY.push(this.y);
@@ -37,20 +39,50 @@ class Agent {
         if (this.pastX.length > 10) this.pastX.shift();
         if (this.pastY.length > 10) this.pastY.shift();
 
-        this.resistance += this.alea();
-        this.resistance = Agent.mod(this.resistance, 1.0);
+        this.resistance += 0.05 * (this.alea() - 0.5);
+
+        if (this.resistance < 0) this.resistance = 0.2 * this.alea();
+        if (this.resistance > 1) this.resistance = 0.2 * this.alea() + 0.8;
+        this.resistance = Math.min(Math.max(this.resistance, 0), 1.0);
     }
 
     draw() {
-        for (var i = 0; i < this.pastX.length; i++) {
-            var adjustedX = this.pastX[i] * this.canvas.width;
-            var adjustedY = this.pastY[i] * this.canvas.height;
+        ctx.beginPath();
 
-            this.ctx.fillRect(adjustedX,adjustedY,3,3);
+        var x = this.pastX[0];
+        var y = this.pastY[0];
+
+        ctx.moveTo(x * this.canvas.width, y * this.canvas.height);
+
+        for (var i = 1; i < this.pastX.length; i++) {
+            var newX = this.pastX[i];
+            var newY = this.pastY[i];
+
+            if (this.checkLine(x, newX) && this.checkLine(y, newY)) {
+                ctx.lineTo(newX * this.canvas.width, newY * this.canvas.height);
+            }
+            else {
+                ctx.stroke();
+                ctx.moveTo(newX * this.canvas.width, newY * this.canvas.height);
+            }
+
+            x = newX;
+            y = newY;
         }
+
+        ctx.strokeStyle = this.color;
+        ctx.stroke();
     }
 
-    static mod(n, m) {
+    mod(n, m) {
         return ((n % m) + m) % m;
+    }
+
+    // Make sure that a line from a to b didn't cross the screen boundary.
+    checkLine(a, b) {
+        if (a < 0.2 && b > 0.8) return false;
+        if (b < 0.2 && a > 0.8) return false;
+
+        return true;
     }
 }
--- js/main.js
+++ js/main.js
@@ -6,19 +6,48 @@ var agents = [];
 var fps = 60;
 var offsetX = 0;
 var offsetY = 0;
-var offsetInc = 0.004;
+var offsetInc = 0.002;
+var palettes = {
+    blue: [
+        '#FAFAFA',
+        '#C7EEFF',
+        '#0077C0',
+        '#1D242B'
+    ],
+    lemonade: [
+        '#F7E74A',
+        '#09C6AB',
+        '#068888',
+        '#02556D'
+    ],
+    cyborg: [
+        '#3C2F3D',
+        '#2EAC6D',
+        '#9DDA52',
+        '#F0F0F0'
+    ],
+    acid: [
+        '#041122',
+        '#259073',
+        '#7FDA89',
+        '#E6F99D'
+    ]
+};
 
 function init() {
+    var palette = palettes.acid;
+
     canvas = document.getElementById("cnvs");
     ctx = this.canvas.getContext("2d");
+    canvas.style.backgroundColor = palette[0];
 
     resize();
 
     var alea = new Alea();
     var simplex = new SimplexNoise(alea);
 
-    for (var i = 0; i < 2000; i++) {
-        agents.push(new Agent(simplex, alea, canvas));
+    for (var i = 0; i < 3000; i++) {
+        agents.push(new Agent(simplex, alea, canvas, palette.slice(1, palette.length)));
     }
 
     draw();
